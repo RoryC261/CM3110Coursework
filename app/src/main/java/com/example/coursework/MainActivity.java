464a0e99b26e82cdc760e86c8d106cba03dc7d8e
roryc261 <rorycameron75@gmail.com>
2022-12-01 00:50:34 +0000

    Next movie button dsplays next movie on reccomendation screen

--- app/src/main/java/com/example/coursework/MainActivity.java
+++ app/src/main/java/com/example/coursework/MainActivity.java
@@ -23,7 +23,6 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RequestQueue mQueue;
     private EditText mEnterArtistResult;
 
 
@@ -33,64 +32,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button buttonParse = findViewById(R.id.button_parse);
-        mQueue = Volley.newRequestQueue(this);
 
         mEnterArtistResult = (EditText)findViewById(R.id.enterArtist);
 
         Button buttonRec = findViewById(R.id.button_rec);
 
         buttonParse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                jsonParse();
-            }
-        });
-
-        buttonRec.setOnClickListener(new View.OnClickListener() {
+            // STOP CONTROL Z HERE PLS
             @Override
             public void onClick(View view) {
                 openMovieRec();
             }
         });
-
     }
 
     public void openMovieRec() {
+        Log.i("====== DEBUG ", "LOG 1 - BUTTON LISTENER ======");
         Intent openMovieRec = new Intent(this, MovieRecPage.class);
+        openMovieRec.putExtra("user_input", mEnterArtistResult.getText().toString());
         startActivity(openMovieRec);
-    }
-
-    private void jsonParse() {
-        String key = "445164-RoryCame-WLB5WH2V";
-        String url = "https://tastedive.com/api/similar?info=1&q=" + mEnterArtistResult.getText() + "&k=" + key;
-        Log.println(Log.DEBUG, "debug", "=== API TEST 1 ===");
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.println(Log.DEBUG, "debug", "=== API TEST 2 ===");
-                        try {
-                            Log.println(Log.DEBUG, "debug", "=== API TEST 3 ===");
-                            JSONObject jsonObject = response.getJSONObject("Similar");
-                            JSONArray jsonArray = jsonObject.getJSONArray("Results");
-                            Log.println(Log.DEBUG, "debug", "=== API TEST 4 ===");
-
-                            Intent searchResults = new Intent(MainActivity.this, MovieRecPage.class);
-                            searchResults.putExtra("search_results", jsonArray.toString());
-                            Log.println(Log.DEBUG, "debug", "=== API TEST 5 ===");
-                            openMovieRec();
-
-                        } catch (JSONException e) {
-                            Log.println(Log.DEBUG, "debug", "=== API TEST 6 ===");
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-            }
-        });
-        mQueue.add(request);
     }
 }
--- app/src/main/java/com/example/coursework/MovieRecPage.java
+++ app/src/main/java/com/example/coursework/MovieRecPage.java
@@ -2,10 +2,18 @@ package com.example.coursework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,34 +22,81 @@ import org.json.JSONObject;
 public class MovieRecPage extends AppCompatActivity {
 
     private TextView mTextViewResult;
+    private RequestQueue mQueue;
+    private int counter = 0;
+    private JSONArray jsonArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_rec_page);
 
+        mQueue = Volley.newRequestQueue(this);
         mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonNext = findViewById(R.id.button_next);
 
-        Intent intent = getIntent();
-        String jsonArray = intent.getStringExtra("search_results");
-        JSONArray searchResults = new JSONArray(jsonArray);
-        Log.println(Log.DEBUG, "debug", "=== API TEST 7 ===");
-        // mTextViewResult.append("HELLO WORLD");
+        Bundle movieResults = getIntent().getExtras();
+        String userInput = movieResults.getString("user_input");
+        Log.i("====== DEBUG ", "LOG 2 - INTENT GET() IS NOT THROWING ERROR ======");
 
+        String key = "445164-RoryCame-WLB5WH2V";
+        String url = "https://tastedive.com/api/similar?info=1&q=" + userInput + "&k=" + key;
 
-        try{
-            for (int i = 0; i < searchResults.length(); i++) {
-                JSONObject movie = searchResults.getJSONObject(i);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject jsonObject = response.getJSONObject("Similar");
+                            jsonArray = jsonObject.getJSONArray("Results");
 
-                String name = movie.getString("Name");
-                String type = movie.getString("Type");
+                            JSONObject movie = jsonArray.getJSONObject(getCounter());
 
-                mTextViewResult.append(name + ", " + type + "\n\n");
+                            nextSong();
+                            Log.i("====== DEBUG ", "LOG 7 - COUNTER INITIALLY INCREMENTED ======");
+                            Log.i("====== DEBUG ", "LOG 3 - API RESULT IS INITIALLY DISPLAYED ======");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        });
+        mQueue.add(request);
 
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Log.i("====== DEBUG ", "LOG 4 - WE ARE IN THE EVENT LISTENER ======");
+                    nextSong();
+                    Log.i("====== DEBUG ", "LOG 8 - GOT PAST INCREMENTATION ======");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
+    }
+    private int getCounter(){
+        return counter;
+    }
+
+    private void setCounter(int value){
+        counter = counter + value;
+    }
+
+    public void nextSong() throws JSONException {
+        Log.i("====== DEBUG ", "LOG 5 - WE ARE SETTING NEW RESULTS TO BE DISPLAYED AFTER BUTTON PRESSED ======");
+        JSONObject movie = jsonArray.getJSONObject(getCounter());
+        String movieName = movie.getString("Name");
+        String movieDescription = movie.getString("wTeaser");
+        String movieTrailer = movie.getString("yUrl");
+        mTextViewResult.setText(movieName + ", " + movieDescription + movieTrailer + "\n\n");
+        setCounter(1);
+        Log.i("====== DEBUG ", "LOG 6 - RESULTS HOPEFULLY DISPLAYED ======");
     }
 }
